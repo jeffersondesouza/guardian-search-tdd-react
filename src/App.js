4f36130d8ce1bb418627f8258d50b90c517cf6e6
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-04-11 19:06:33 -0300

    init app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchContainer from './containers/SearchContainer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div >
+        <header>
+          <h1>The Guardian Serach App</h1>
         </header>
+        <SearchContainer />
       </div>
     );
   }
 }
 
 export default App;
+
+/*
+  Hello from the Guardian. Thank you for registering with the open platform. A new key has been created for you: 4f0dc017-ba27-4650-ba21-ea7b65fb88c6 You can try this key by accessing https://content.guardianapis.com/search?api-key=4f0dc017-ba27-4650-ba21-ea7b65fb88c6 in your browser. For more details on how to use the open platform API, check out the documentation available at http://open-platform.theguardian.com/documentation/
+*/
---
+++ src/containers/SearchContainer/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import React, { Component } from 'react';
+
+const SearchContainer = ()=> <div>SearchContainer</div>;
+
+export default SearchContainer;
