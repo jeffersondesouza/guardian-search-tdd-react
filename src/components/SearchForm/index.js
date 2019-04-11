5efcdc19a7b1e88ad3fe83b6108100e61b7114df
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-04-11 19:25:30 -0300

    form events ok

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SearchForm = () => (
-  <div>
-    <form>
-      <input placeholder="Procure uma matéria" />
-    </form>
-  </div>
-);
+const SearchForm = () => {
+  const [state, setState] = useState({ value: '' });
+  
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setState({
+      ...state,
+      value
+    })
+  }
 
+  const handleSubmit = e =>{
+    e.preventDefault();
+    console.log(state);
+    
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Procure uma matéria"
+          value={state.value}
+          onChange={handleChange}
+        />
+      </form>
+      <div>
+        {JSON.stringify(state, null, 2)}
+      </div>
+
+    </div>
+  );
+}
 export default SearchForm;
